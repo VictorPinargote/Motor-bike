f0bcaa144792fa8625ee9fac1a31fd94cdfa4fe1
Romeorobles <nicolrobles423@gmail.com>
2026-06-16 07:45:32 -0500

    Configurar TypeORM, Mongoose y GitHub Actions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -7,7 +10,34 @@ import { MotosModule } from './motos/motos.module';
 import { VentasModule } from './ventas/ventas.module';
 
 @Module({
-  imports: [AuthModule, ClientesModule, MotosModule, VentasModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASS'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    ClientesModule,
+    MotosModule,
+    VentasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
